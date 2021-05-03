a5094c37e8967d0104e4866f234f56366f61df42
Camila ZG <camilazg@iMac-de-Jose.local>
2021-05-03 19:44:39 -0400

    counter_app_v1

---
+++ counter.js
@@ -0,0 +1,29 @@
+// according to NodeJs literature, first you must run command
+// "npm install prompt-sync" on terminal
+
+const prompt = require('prompt-sync')();
+
+function view(counter) {
+        return(`      Count ${counter}\n\n      (+) (-)\n\n      (q) for quit\n\n`);
+}
+
+function update(msg, counter) {
+    if (msg=='+') return counter+1;
+    if (msg=='-') return counter-1;
+    return counter;
+}
+
+function app(counter) {
+    while(true) {
+    const currentView= view(counter);
+    console.clear();
+    console.log(currentView);
+    const msg= prompt("What would you do? ");
+    if (msg=="q") break;
+    counter=update(msg,counter);
+    }
+}
+
+app(0);
+
+
